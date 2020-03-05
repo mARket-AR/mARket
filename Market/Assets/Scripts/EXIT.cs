9b863a6de1f98af7db7916e30f0c079d91652a8d
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2020-03-05 12:22:34 +0200

    new model

---
+++ Market/Assets/Scripts/EXIT.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXIT : MonoBehaviour
+{
+    void doExitGame()
+    {
+        Application.Quit();
+    }
+}
